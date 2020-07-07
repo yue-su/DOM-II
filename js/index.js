94b5054e18a2e0e568afbe984ffb0db5390266bd
Yue Su <yuesu@Mac-Pro.local>
2020-07-07 16:18:08 -0300

    mouseover/mouseleave

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+// mouseover mouseleave
+let headerImg = document.querySelector('.intro img')
+headerImg.addEventListener('mouseover', fade)
+function fade(event) {
+    event.target.style.opacity = '60%'
+}
+
+headerImg.addEventListener('mouseleave', fadeReset)
+function fadeReset(event) {
+    event.target.style.opacity = '100%'
+}
+
