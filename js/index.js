d2f55472b9fe4d86bdcdba36107f80f44953eac9
Yue Su <yuesu@Mac-Pro.local>
2020-07-08 16:10:42 -0300

    adding gsap animation

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// 1 mouseover 
+//  mouseover 
 let headerImg = document.querySelector('.intro img')
 
 headerImg.addEventListener('mouseover', Fade)
@@ -6,13 +6,13 @@ function Fade(event) {
     event.target.style.opacity = '60%'
 }
 
-//2 mouseleave
+// mouseleave
 headerImg.addEventListener('mouseleave', FadeReset)
 function FadeReset(event) {
     event.target.style.opacity = '100%'
 }
 
-//3 keydown
+// keydown
 let body = document.querySelector('body')
 body.addEventListener('keydown', Popup)
 const popKey = document.createElement('h1')
@@ -21,13 +21,13 @@ function Popup(event) {
     body.prepend(popKey)
 }
 
-//4 keyup
+// keyup
 body.addEventListener('keyup', Remove)
 function Remove(event) {
     popKey.remove()
 }
 
-//5 wheel
+// wheel
 //rotate the image when mouse hovering on
 let contentImg = document.querySelectorAll('.content-section img')
 contentImg.forEach((item) => item.addEventListener('wheel', rotate))
@@ -36,16 +36,42 @@ function rotate(event) {
     event.target.style.transform = `rotate(${event.deltaY}deg)`
 }
 
-//6 click
+// click
+let des = document.querySelectorAll('.destination')
+des.forEach((item) => { item.addEventListener('click', AddBorder) })
+function AddBorder(event) {
+    event.target.style.border = '5px dotted red'
+}
+
+// copy
+// show alert - Do not Copy
+let bodyText = document.querySelectorAll('p')
+bodyText.forEach((item)=> item.addEventListener('copy', ()=> alert('Do not copy')))
+
+// dblclick
+let mainTitle = document.querySelector('h1')
+mainTitle.addEventListener('dblclick', (event) => event.target.style.fontSize = "700%")
+
+//9 resize
+let homeWindow = document.querySelector('.container p')
+homeWindow.addEventListener('resize', (event) => event.target.style.color = `Red`)
+
+
+//Stop the nav items from refreshing the page
+let navItems = document.querySelectorAll('nav a')
+navItems.forEach((item)=>item.addEventListener('click', (event)=>event.preventDefault()))
+
+//stopPropagation
 let btn = document.querySelectorAll('.btn')
 btn.forEach((item) => item.addEventListener('click', ChangeColor))
 function ChangeColor(event) {
+    //adding stopPropagation so the des handler won't be affected
+    event.stopPropagation()
     event.target.style.backgroundColor = 'grey'
 }
 
-//7 copy
+//stretch
+//rotate the header 360deg
+gsap.to('img', { duration: 3, rotation: 360 })
 
-//8 dblclick
-
-
-
+//
