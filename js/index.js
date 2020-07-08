b0cf34d4245bf4a31e2e41511b8ebe9f28690534
Yue Su <yuesu@Mac-Pro.local>
2020-07-07 21:01:26 -0300

    keydown / wheel

--- js/index.js
+++ js/index.js
@@ -1,12 +1,35 @@
 // mouseover mouseleave
 let headerImg = document.querySelector('.intro img')
-headerImg.addEventListener('mouseover', fade)
-function fade(event) {
+
+headerImg.addEventListener('mouseover', Fade)
+function Fade(event) {
     event.target.style.opacity = '60%'
 }
 
-headerImg.addEventListener('mouseleave', fadeReset)
-function fadeReset(event) {
+headerImg.addEventListener('mouseleave', FadeReset)
+function FadeReset(event) {
     event.target.style.opacity = '100%'
 }
+
+//keydown
+let body = document.querySelector('body')
+body.addEventListener('keydown', Popup)
+const popKey = document.createElement('h1')
+function Popup(event) {
+    popKey.textContent = event.key
+    body.prepend(popKey)
+}
+
+//wheel
+//rotate the image when mouse hovering on
+let contentImg = document.querySelectorAll('.content-section img')
+contentImg.forEach((item) => item.addEventListener('wheel', rotate))
+function rotate(event) {
+    event.preventDefault()
+    event.target.style.transform = `rotate(${event.deltaY}deg)`
+}
+
+
+//drag / drop
+
 
